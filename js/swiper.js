6f626a351a6d73f2b11809d18017caec717da67d
Lada Penkova <whitethe2001@gmail.com>
2021-11-18 20:02:23 +0300

    Hero pixel perfect

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
+  speed: 5000,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 3000,
+  },
 
   effect: 'fade',
   fadeEffect: {
